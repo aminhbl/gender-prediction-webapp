04a0a4fc252f5d2c1c2462994c311d9289cc63cd
aminhbl <amin.habibllh@gmail.com>
2021-12-03 07:55:53 +0330

    feat: Add docs

--- js/jscript.js
+++ js/jscript.js
@@ -1,9 +1,13 @@
-
-
+/**
+ * This function handles all the events  related to 'submit' button.
+ * At first name would be validated, then HTTP request would be sent by fetch and promise,
+ * if response is ok, then gender and probability would be shown.
+ * @param event
+ */
 function submitName(event) {
     let name = validateName(event)
     if (name) {
-
+        // To see if result for this name already saved in local storage?
         let saved_gender = localStorage.getItem(name)
         if (saved_gender != null) {
             document.getElementById('saved_gender').style.visibility = 'visible'
@@ -18,6 +22,7 @@ function submitName(event) {
         fetch(url)
             .then((response) => {
                 if (!response.ok) {
+                    //error raised if response is not ok
                     throw new Error(
                         "Network response was not ok, " + response.status
                     );
@@ -26,10 +31,12 @@ function submitName(event) {
             })
             .then((content) => {
                 if (content['gender'] != null) {
+                    // update the Gender and Probability
                     document.getElementById('predicted_gender').innerHTML = `Gender: ${content['gender']}`
                     document.getElementById('predicted_probability').innerHTML = `Probability: ${content['probability']}`
                 }
                 else {
+                    // raise an error for name not considered valid by genderize
                     document.getElementById('predicted_gender').innerHTML = `Gender:`
                     document.getElementById('predicted_probability').innerHTML = `Probability:`
                     raiseFlag('Genderize does not consider your name valid!')
@@ -45,6 +52,13 @@ function submitName(event) {
 
 }
 
+/**
+ * This function handles all the events  related to 'save' button.
+ * At first name would be validated, then if radio button is checked, name would be saved with
+ * specified gender by that radio, if not name would be saved with predicted gender, and if there is not predicted
+ * gender, then an error would be raised.
+ * @param event
+ */
 function saveName(event) {
     let name = validateName(event)
     let predicted_gender = document.getElementById('predicted_gender').innerHTML.split(" ")
@@ -69,6 +83,12 @@ function saveName(event) {
     }
 }
 
+/**
+ * This function handles all the events  related to 'clear' button.
+ * At first name would be validated, then considering that this button would show up only if
+ * there is a record for that name in local storage we can safely remove the record.
+ * @param event
+ */
 function clearName(event) {
     let name = validateName(event)
     if (name) {
@@ -79,6 +99,12 @@ function clearName(event) {
     }
 }
 
+/**
+ * Iterate on radio buttons, return the value of the one that is checked
+ * or return null.
+ * @param event
+ * @returns {null|*}
+ */
 function validateGender(event) {
     let radios = document.getElementsByName('gender')
     for (let i = 0; i < radios.length; i++) {
@@ -89,6 +115,13 @@ function validateGender(event) {
     return null
 }
 
+/**
+ * Validate the name to be less than 255 characters and only include letters and space.
+ * If not raise an appropriate error and prevent the submit button to perform as its default.
+ * If validated return the value of name.
+ * @param event
+ * @returns {null|*}
+ */
 function validateName(event) {
     let name = document.getElementById('name').value
     if (name.length > 255) {
@@ -107,6 +140,11 @@ function validateName(event) {
     return name
 }
 
+/**
+ * Raise the error and messaging bar named flag, which sits on top of the main container.
+ * Show the flag for 4.5 seconds and then roll it down by reducing the height.
+ * @param error
+ */
 function raiseFlag(error) {
     document.getElementById("flag").style.padding = "2px";
     document.getElementById("flag").style.height = "30px";
@@ -119,6 +157,7 @@ function raiseFlag(error) {
     }, 4500);
 }
 
+// register of handling functions
 document.getElementById('submit').onclick = submitName
 document.getElementById('save').onclick = saveName
 document.getElementById('clear').onclick = clearName
